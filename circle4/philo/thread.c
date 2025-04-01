c8f3f97102d18b2c411f2f6080c4d53d5f074154
Martin Arsenijevic <martin.arsenijevic@gmail.com>
2025-04-01 18:43:04 +0200

    fixed memory leak

--- circle4/philo/thread.c
+++ circle4/philo/thread.c
@@ -6,11 +6,50 @@
 /*   By: marsenij <marsenij@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 15:06:02 by marsenij          #+#    #+#             */
-/*   Updated: 2025/03/29 19:04:22 by marsenij         ###   ########.fr       */
+/*   Updated: 2025/04/01 18:41:18 by marsenij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	join_threads(t_data *data)
+{
+	int		i;
+	t_philo	*current;
+
+	current = data->philo_head;
+	i = -1;
+	while (++i < data->num_philo)
+	{
+		pthread_join(current->thread, NULL);
+		current = current->next;
+	}
+}
+
+void	destroy_mutex(t_data *data)
+{
+	int		i;
+	t_philo	*current;
+	t_philo	*next;
+
+	current = data->philo_head;
+	i = -1;
+	while (++i < data->num_philo)
+		pthread_mutex_destroy(&data->forks[i]);
+	free(data->forks);
+	i = -1;
+	while (++i < data->num_philo)
+	{
+		pthread_mutex_destroy(&current->should_eat_mutex);
+		pthread_mutex_destroy(&current->meal_mutex);
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	pthread_mutex_destroy(&data->start_mutex);
+	pthread_mutex_destroy(&data->died_mutex);
+	pthread_mutex_destroy(&data->print_mutex);
+}
 
 int	malloc_and_init_mutex(t_data *data)
 {
@@ -42,56 +81,38 @@ int	create_threads(t_data *data)
 	{
 		if (pthread_create(&current->thread, NULL,
 				philosopher_routine, current) != 0)
+		{
+			pthread_mutex_unlock(&data->start_mutex);
+			while (i-- > 0)
+			{
+				current = data->philo_head;
+				pthread_join(current->thread, NULL);
+				current = current->next;
+			}
+			destroy_mutex(data);
 			return (0);
+		}
 		current = current->next;
 	}
 	data->meal_monitor_thread = create_meal_thread(data);
 	if (!data->meal_monitor_thread)
+	{
+		pthread_mutex_unlock(&data->start_mutex);
+		join_threads(data);
+		destroy_mutex(data);
 		return (0);
+	}
 	data->death_monitor_thread = create_death_thread(data);
 	if (!data->death_monitor_thread)
+	{
+		pthread_mutex_unlock(&data->start_mutex);
+		join_threads(data);
+		destroy_mutex(data);
 		return (0);
+	}
+	data->threads_created = 1;
 	pthread_mutex_unlock(&data->start_mutex);
 	return (1);
-}
-
-void	join_threads(t_data *data)
-{
-	int		i;
-	t_philo	*current;
-
-	current = data->philo_head;
-	i = -1;
-	while (++i < data->num_philo)
-	{
-		pthread_join(current->thread, NULL);
-		current = current->next;
-	}
-}
-
-void	free_threads(t_data *data)
-{
-	int		i;
-	t_philo	*current;
-	t_philo	*next;
-
-	current = data->philo_head;
-	i = -1;
-	while (++i < data->num_philo)
-		pthread_mutex_destroy(&data->forks[i]);
-	free(data->forks);
-	i = -1;
-	while (++i < data->num_philo)
-	{
-		pthread_mutex_destroy(&current->should_eat_mutex);
-		pthread_mutex_destroy(&current->meal_mutex);
-		next = current->next;
-		free(current);
-		current = next;
-	}
-	pthread_mutex_destroy(&data->start_mutex);
-	pthread_mutex_destroy(&data->died_mutex);
-	pthread_mutex_destroy(&data->print_mutex);
 }
 
 void	initialize_threads(t_data *data)
@@ -112,5 +133,5 @@ void	initialize_threads(t_data *data)
 		pthread_join(data->death_monitor_thread, NULL);
 	if (data->has_to_eat_x > 0)
 		pthread_join(data->meal_monitor_thread, NULL);
-	free_threads(data);
+	destroy_mutex(data);
 }
